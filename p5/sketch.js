04db29276fa388fb5d331ac32bf1ac49b3c83cb6
paoloVernocchi95 <43385409+paoloVernocchi95@users.noreply.github.com>
2018-10-12 10:02:03 +0200

    prova venerdi mattina

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,64 +1,54 @@
+var img;
 function preload(){
   // put preload code here
+
 }
-/*var myBall = {};
-var secondBall = {};*/
-var balls = [];
+var myCuteBall;
 function setup() {
+createCanvas(windowWidth, windowHeight);
+myCuteBall = new Ball(width/2, height/2, 50);
 
-  createCanvas(windowWidth, windowHeight);
 
-  var ballNumber = 70;
-  for (var i=0; i<ballNumber;i++) {
-    var myBall = new Ball(random(0,width),random(0,height),10);
 
-    balls.push(myBall);
-  }
-  /*  var myEdge = edgeFromArea(100);
-    console.log(myEdge);*/
-  /*myBall = new Ball(150, 200, 60);
-  secondBall= new Ball (100,500,50);
-  myBall.diameter = 50;
-  myBall.color = ('green');
-  myBall.speed=8;
-  secondBall.speed=3;
-  myBall.display();*/
+
 }
+
 
 function draw() {
-  for (var j=0; j<balls.length;j++){
-    balls[j].move();
-    balls[j].display();
-    balls[j].color = color(random(255),random(255),random(255));
-  }
-/*  myBall.move();
-  myBall.display();
-  secondBall.move();
-  secondBall.display();*/
+  background(200);
+  myCuteBall.move();
+    myCuteBall.display();
+
+
+
 }
-//my object
 function Ball(_x, _y, _diameter) {
+  // Properties defined by constructor
+  this.size = _diameter;
   this.x = _x;
   this.y = _y;
-  this.diameter = _diameter;
-  this.color = 'Orange'//(random(0,275),random(0,275),random(0,275));
-  this.speed = 2;
-  var yDir=random(0,5);
-  var xDir=random(0,5);
+  // Hardcoded properties
+  this.color = 'red';
+  this.speedx = 1;
+  this.speedy = 1;
+  // Methods
+var xDir=1;
+var yDir=1;
 
-  this.display = function(){
+
+
+  this.display = function() {
     fill(this.color);
-    ellipse(this.x, this.y, this.diameter);
+    ellipse(this.x, this.y, this.size);
   }
   this.move = function(){
-    this.x +=this.speed*xDir;
-    this.y +=this.speed *yDir;
-    if (this.y>height || this.y<0) {
-      yDir = yDir*(-1);
-    }
-    if (this.x>width || this.x<0) {
-      xDir = xDir*(-1);
-    }
+  this.x +=(5*this.speedx*this.speedx/2+5*this.speedx)*xDir;
+  this.y +=this.speedy *yDir;
+  if (this.y>height || this.y<0) {
+    yDir = yDir*(-1);
   }
-
+  if (this.x>width || this.x<0) {
+    xDir = xDir*(-1);
+  }
+}
 }
